571b5ee62e56ab3e447f0751c8ad2ded13f122f2
Kcodem <krisham3579@gmail.com>
2026-09-15 11:59:05 -0400

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,11 @@
+public class HelloWorld
+{
+    public static void main(String[] args)
+    {
+        System.out.println("|-----------------------------------------|");
+        System.out.println("| My name is Krisha Maharjan              |");
+        System.out.println("| I've been troubleshooting Intellij...   |");
+        System.out.println("| FOR 2 HOURS. KILL ME.                   |");
+        System.out.println("|-----------------------------------------|");
+    }
+}
